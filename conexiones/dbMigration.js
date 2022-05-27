b2d30273fef220668f3af7d38d5e72268370ac3f
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-27 01:08:45 -0500

    Crea alumno

--- conexiones/dbMigration.js
+++ conexiones/dbMigration.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 let mrp = {
   host     : 'localhost',
-  port     : '3307',
-  user     : 'root',
-  password : 'root',
+  port     : '3306',
+  user     : 'mrp',
+  password : 'mrpdatabase',
   database:  'mrp',
   multipleStatements: true
 };
--- routes/rmp/creaUsuarios.js
+++ routes/rmp/creaUsuarios.js
@@ -23,7 +23,7 @@ module.exports = {
         let usuario = req.body.usuario
         let password = req.body.password
 
-        let query = `INSERT INTO PROFESORES (NOMBRE_PROFESOR, CORREO, USUARIO, PASSWORD)
+        let query = `INSERT INTO ALUMNOS (NOMBRE_ALUMNO, CORREO, USUARIO, PASSWORD)
                     VALUES ?`
 
         if(!f.definido(nombre) || !f.definido(correo) || !f.definido(usuario) || !f.definido(password)) {
@@ -57,7 +57,48 @@ module.exports = {
     }, 
 
     creaAlumno: async (req, res) => {
-        
+        let response = {
+            replyCode: 200,
+            replyText: "Usuario creado",
+            data: []
+        }
+
+        let nombre = req.body.nombre
+        let correo = req.body.correo
+        let usuario = req.body.usuario
+        let password = req.body.password
+
+        let query = `INSERT INTO ALUMNOS (NOMBRE_ALUMNOS, CORREO, USUARIO, PASSWORD)
+                    VALUES ?`
+
+        if(!f.definido(nombre) || !f.definido(correo) || !f.definido(usuario) || !f.definido(password)) {
+            response.replyCode = 500;
+            response.replyText = 'Error en la solicitud de datos';
+            response.data = undefined;
+            res.status(500).send(response);
+        } else {
+            let inserta = []
+            inserta.push([
+                nombre,
+                correo,
+                usuario,
+                password
+            ])
+            dbMrp.query(query, [inserta], async (err, data) => {
+                if(err) {
+                    console.log(err)
+                    response.replyCode = 500;
+                    response.replyText = 'Error en la solicitud de datos';
+                    response.data = undefined;
+                    res.status(500).send(response);
+                } else {
+                    response.replyCode = 200;
+                    response.replyText = 'Usuario creado con exito';
+                    response.data = [data.insertId];
+                    res.status(200).send(response);
+                }
+            })
+        }
     }
 
 }
