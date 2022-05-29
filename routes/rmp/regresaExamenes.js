a03f100f7ca43c1a93ca5647990d7191230e6884
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 18:04:31 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -33,8 +33,8 @@ module.exports = {
         }
 
         let letra = req.params.letra.toString()
-        let expresion = '/(\b[@])/'
-        let regFinal = new RegExp('@', 'gi')
+        let expresion = '/^letra/'
+        let regFinal = new RegExp('letra', 'gi')
         let regFinalFinal = expresion.replace(regFinal, letra)
         console.log(regFinalFinal)
 
