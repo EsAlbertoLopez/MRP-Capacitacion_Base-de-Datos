3899313a623c9f8e0ecc3513f6f563595f4594a1
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 14:38:17 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -34,6 +34,7 @@ module.exports = {
                         res.status(500).send(response);
                     } else {
                         db.close();
+                        console.log(result[0])
                         response.replyCode = 200;
                         response.replyText = 'Examen recuperado con exito';
                         response.data = [result];
