6e70b2074d9b19e153fa1e645e529d643ec3ac27
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 15:49:29 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -41,8 +41,8 @@ module.exports = {
             mongoose.connect(url, async function(err, db) {
                 let examenesResultado = await modelo.examen.find({"examen.materia": /(\b[Rr])/}).exec()
                 console.log(examenesResultado)
-                examenesResultado = examenesResultado.map((obj) => ({
-                    id: obj['examen'][0],
+                examenesResultado = examenesResultado.forEach((obj) => ({
+                    id: obj.examen.id,
                     nombreExamen: obj.nombreExamen,
                     idMateria: obj.idMateria,
                     materia: obj.materia,
