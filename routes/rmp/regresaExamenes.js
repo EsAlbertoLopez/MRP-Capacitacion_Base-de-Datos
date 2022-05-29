13c6535e2ee8258360e2edbb5cff8b76dc70b670
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 18:05:12 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -45,7 +45,7 @@ module.exports = {
             res.status(500).send(response);
         } else {
             mongoose.connect(url, async function(err, db) {
-                let examenesResultado = await modelo.examen.find({"examen.materia":{$regex: regFinalFinal}}).lean().exec()
+                let examenesResultado = await modelo.examen.find({"examen.materia":{$regex: /^R/}}).lean().exec()
                 console.log(examenesResultado)
             })
         }
