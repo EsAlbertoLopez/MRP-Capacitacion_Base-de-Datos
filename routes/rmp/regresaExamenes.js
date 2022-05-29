c0a7243378960cd2838780359a9e8229a979eae9
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 13:46:28 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -25,7 +25,7 @@ module.exports = {
             res.status(500).send(response);
         } else {
             mongoose.connect(url, function(err, db) {
-                db.collection("EXAMENES").find({"examen.materia": /^R/, "limit": 10}, function(err, result) {
+                db.collection("EXAMENES").find({"examen.materia": /(\b[Rr])/}, function(err, result) {
                     if(err) {
                         db.close();
                         response.replyCode = 500;
