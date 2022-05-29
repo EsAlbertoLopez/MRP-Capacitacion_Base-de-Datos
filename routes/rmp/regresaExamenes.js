990bcb05838800a5452d89c5400cc9b937f95c1d
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-05-29 16:05:11 -0500

    Regresa examenes

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -39,17 +39,17 @@ module.exports = {
             res.status(500).send(response);
         } else {
             mongoose.connect(url, async function(err, db) {
-                let examenesResultado = await modelo.examen.find({"examen.materia": /(\b[Rr])/}).exec()
+                let examenesResultado = await modelo.examen.find({"examen.materia": /(\b[Rr])/}, {"_id": 0, "examen.preguntas": 0}).exec()
                 console.log(examenesResultado)
-                examenesResultado.forEach(obj => {
+                examenesResultado.map(obj => {
                     console.log(obj)
-                    // id: obj.examen.materia,
-                    // nombreExamen: obj.nombreExamen,
-                    // idMateria: obj.idMateria,
-                    // materia: obj.materia,
-                    // profe: obj.profe,
-                    // dificultad: obj.dificultad,
-                    // cobro: obj.cobro
+                    id: obj.examen.materia,
+                    nombreExamen: obj.nombreExamen,
+                    idMateria: obj.idMateria,
+                    materia: obj.materia,
+                    profe: obj.profe,
+                    dificultad: obj.dificultad,
+                    cobro: obj.cobro
                 });
                 console.log(examenesResultado)
             })
