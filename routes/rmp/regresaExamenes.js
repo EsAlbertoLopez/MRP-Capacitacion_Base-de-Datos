373a3dfd5fd58e7dca30f0e0666b06621048bb88
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-06-02 23:25:41 -0500

    Cambio codigo

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -69,7 +69,7 @@ module.exports = {
         let palabraMayuscula = palabra.toUpperCase()
         let palabraMinuscula = palabra.toLowerCase()
         let palabraPrimeraLetra = capitalize(palabra)
-        let re = new RegExp(`/${palabraMayuscula}|${palabraMinuscula}|${palabraPrimeraLetra}/gi`);
+        let re = new RegExp(`/${palabraMayuscula}|${palabraMinuscula}|${palabraPrimeraLetra}/ig`);
 
         if(!f.definido(palabra)) {
             response.replyCode = 500;
