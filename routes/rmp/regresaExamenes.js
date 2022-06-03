78a9b514c05a9d5c7c93a84dea5a3b22701b027d
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-06-02 23:21:27 -0500

    Cambio codigo

--- routes/rmp/regresaExamenes.js
+++ routes/rmp/regresaExamenes.js
@@ -62,7 +62,7 @@ module.exports = {
         }
 
         let palabra = req.params.palabra.toString()
-        let re = new RegExp(`/${palabra}/ig`);
+        let re = new RegExp(`${palabra}/ig`);
 
         if(!f.definido(palabra)) {
             response.replyCode = 500;
