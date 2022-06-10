c0609eb9e064513764fa2b724083396588ea0d98
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2022-06-09 21:48:34 -0500

    M

--- routes/rmp/rmp.js
+++ routes/rmp/rmp.js
@@ -15,7 +15,7 @@ const { creaAlumno, creaProfesor } = require('./creaUsuarios');
 const { cargaExamen } = require('./cargarExamen');
 const { generaToken, validaToken, generaTokenVip, validaTokenVip } = require('./generaToken');
 const { regresaExamenes, regresaExamen, regresaExamenesPalabras, regresaExamenesMaestro, borrarExamen } = require('./regresaExamenes');
-const { recuperaExamenMuestra } = require=('./recuperaExameneMuestra')
+const { regresaExamenesMuestra } = require('./recuperaExameneMuestra')
 
 //Metodos
 router.get('/obtieneMaterias', obtieneMaterias);
@@ -23,7 +23,7 @@ router.get('/regresaExamenes/:letra', regresaExamenes);
 router.get('/regresaExamen/:idExamen', regresaExamen);
 router.get('/regresaExamenPalabra/:palabra', regresaExamenesPalabras);
 router.get('/regresaExamenMaestro/:idMaestro', regresaExamenesMaestro);
-router.get('recuperaExamenMuestra', rec)
+router.get('recuperaExamenMuestra', regresaExamenesMuestra)
 router.post('/validaUsuarioProfesor', validaUsuarioProfesor);
 router.post('/borraExamen', borrarExamen);
 router.post('/validaUsuarioAlumno', validaUsuarioAlumno);
